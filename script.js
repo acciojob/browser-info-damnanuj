ca9629a72887867cb3c196a174026e550aab451e
Anuj Kumar <106218844+damnanuj@users.noreply.github.com>
2023-08-23 20:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+const holder = document.getElementById("container")
+
+let navigator = navigator.userAgent;
+let appName = navigator.appName;
+let appVersion = navigator.appVersion;
+
